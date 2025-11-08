633ccf994637a2a6b25230ede20a47ca68f35028
clementeaf <carriagadafalcone@gmail.com>
2025-11-08 11:57:36 -0300

    Fix routes.tsx: change extension and fix type import

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -6,8 +6,10 @@ import type { ReactElement } from 'react';
  */
 function Landing(): ReactElement {
   return (
-    <div className="bg-blue-50/60 flex flex-col items-start justify-start min-h-screen w-full p-4">
-      <div className="bg-gray-200 rounded-[20px] w-full h-[20%]">Navbar</div>
+    <div className="bg-blue-50/60 flex flex-col items-start justify-star h-full w-full p-4 gap-4">
+      <div className="flex items-center bg-gray-200 rounded-[20px] w-full h-[10%] sm:h-[10%] p-4">Hero Navbar</div>
+      <div className="flex flex-col bg-gray-200 rounded-[20px] w-full h-[100%] p-4 items-center justify-center">Hero Section</div>
+      <div className="flex flex-col bg-gray-200 rounded-[20px] w-full h-[30%] p-4 items-center justify-center">Hero Footer</div>
     </div>
   );
 }
